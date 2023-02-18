593e5c6bbcf2242be194ec1b060e5e6ae1a2c6ee
Ugo <ugochukwuudeke@gmail.com>
2023-02-18 15:45:58 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,17 +2,24 @@
 
 /**
  * main - Entry point
- *
+ * Description: Print all combinations of single digit numbers.
+ * Numbers must be seaerated by commas and a space.
+ * You can only use 'putchar' to printto the console.
+ * You can only use 'putchar' up to four times.
+ * You are not allowed to use any variable of type 'char'.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int ch;
-for (ch = '0'; ch <= '9'; ch++)
+for (ch = 0; ch < 10; ch++)
 {
-putchar(ch);
+putchar(ch + '0');
+if (ch < 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
