a26aeba7c1fe2e99c456152ce18f47e359460f4a
Ugo <ugochukwuudeke@gmail.com>
2023-02-17 20:30:06 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char nu;
+char ch;
+for (nu = '0'; nu <= '9'; nu++)
+putchar(nu);	
+for (ch = 'a'; ch <= 'f'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
