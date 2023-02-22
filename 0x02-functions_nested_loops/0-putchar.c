7f738344d96600acda5c87e4f624b695d533c958
Ugo <ugochukwuudeke@gmail.com>
2023-02-21 16:21:42 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *  Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+char a = 95;
+char b = 112;
+char c = 117;
+char d = 116;
+char e = 99;
+char f = 104;
+char g = 97;
+char h = 114;
+putchar(a);
+putchar(b);
+putchar(c);
+putchar(d);
+putchar(e);
+putchar(f);
+putchar(g);
+putchar(h);
+putchar('\n');
+return (0);
+}
