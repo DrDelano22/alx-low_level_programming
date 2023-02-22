c06e79904832ac92837128ec8e350da0588bf148
Ugo <ugochukwuudeke@gmail.com>
2023-02-21 17:32:01 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-int main(void)
+void print_alphabet(void)
 {
-int i;
-for (i = a; i < z; i++)
+int c;
+for (c = 'a'; c < 'z'; c++)
 {
-_putchar(i);
+_putchar(c);
 }
 _putchar('\n');
 return (0);
